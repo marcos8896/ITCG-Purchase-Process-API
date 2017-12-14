4e39768858a4a7d38b1370fa65da611f45cb48ca
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-13 22:27:40 -0600

    First aproach of relations with the models.

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,11 @@
 var server = require('./server');
 var ds = server.dataSources.posgresql_ds;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role',
+               'Concept', 'Subdirection','Department'//,
+              //  'Concept_Requisition',  'Input_output', 
+              //  'Input_output_details', 'Provider', 'Purchase_order', 
+              //  'Purchase_order_Requisition', 'Requisition', 
+              ];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
