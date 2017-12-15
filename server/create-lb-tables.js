b22e9b9544c2e9c0a8ecf2e94f1776641392f830
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-14 20:27:29 -0600

    More models added to array

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,11 +1,13 @@
 var server = require('./server');
 var ds = server.dataSources.posgresql_ds;
 var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role',
-               'Concept', 'Subdirection','Department'//,
-              //  'Concept_Requisition',  'Input_output', 
-              //  'Input_output_details', 'Provider', 'Purchase_order', 
-              //  'Purchase_order_Requisition', 'Requisition', 
+               'Concept', 'Subdirection','Department',
+               'Concept_Requisition',  'Input_output', 
+               'Input_output_details', 'Provider', 'Purchase_order', 
+               'Purchase_order_Requisition', 'Requisition', 'Project', 'Program',
+               'Budget_key', 'Budget_key_details'
               ];
+              
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
