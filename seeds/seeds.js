86562f807a04eacac7abc1086fc25a7cd77d29d2
Marcos Barrera Del Río <marcos@softwareallies.com>
2017-12-18 17:08:16 -0600

    Added getModelsContentFromJSONs for dynamic migrations.

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,42 +1,48 @@
-const models = require('../server/server').models;
-const faker = require('faker/locale/es_MX');
+// const models = require('../server/server').models;
+// const faker = require('faker/locale/es_MX');
+// const loadJsonFile = require('load-json-file');
+// console.log('loadJsonFile: ', loadJsonFile);
+const arrayModels = [];
 
-function seedProvider( records ) {
+function getModelsContentFromJSONs() {
+  const readfiles = require('node-readfiles');
 
-  const promises = [];
-  for (let i = 0; i < records; i++) {
-    promises.push(models.Provider.create({
-      "name": faker.name.findName(),
-      "phone": faker.phone.phoneNumber(),
-      "address": faker.address.streetAddress() + faker.address.city() + faker.address.country(),
-      "email": faker.internet.email()
-    }));
-  }
+  readfiles('./common/models/', { filter: '*.json' }, (err, filename, contents) => {
+    if (err) throw err;
+    arrayModels.push({ name: JSON.parse(contents).name, properties: Object.keys(JSON.parse(contents).properties) });
+  }).then(function (files) {
 
-  return Promise.all(promises);
-
-}
-
-function seedConcept( records ) {
-  
-  const promises = [];
-  for (let i = 0; i < records; i++) {
-    promises.push(models.Concept.create({
-      "description": faker.lorem.sentences(10)
-    }));
-  }
-
-  return Promise.all(promises);
-
-}
-
-function seedAll() {
-  const seedsPromises = [ seedProvider(25), seedConcept(25) ];
-  
-  Promise.all(seedsPromises).then( () => {
-    console.log("Ya estufas.");
-    process.exit();
+    console.log('arrayModels: ', JSON.stringify( arrayModels, null, '  ' ));
+  }).catch(function (err) {
+    console.log('Error reading files:', err.message);
   });
 }
 
-seedAll();
+getModelsContentFromJSONs();
+// function seedModel( model ,records ) {
+
+//   const promises = [];
+//   for (let i = 0; i < records; i++) {
+//     promises.push(models.Provider.create({
+//       "name": faker.name.findName(),
+//       "phone": faker.phone.phoneNumber(),
+//       "address": faker.address.streetAddress() + faker.address.city() + faker.address.country(),
+//       "email": faker.internet.email()
+//     }));
+//   }
+
+//   return Promise.all(promises);
+
+// }
+
+// function seedAll() {
+//   const seedsPromises = [ 
+//     seedModel( "Provider" ) ];
+
+//   Promise.all(seedsPromises).then( () => {
+//     console.log("Ya estufas.");
+//     // process.exit();
+//   });
+// }
+
+// seedAll();
