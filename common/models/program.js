424059dacc970003fc89d42fd0a52ae2f0a3dbeb
Diego Moreno <diegomoreno@Diegos-MacBook-Air.local>
2017-12-18 19:06:57 -0600

    Added validation program model

--- common/models/program.js
+++ common/models/program.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Program) {
+    Program.validatesUniquenessOf('program_number', {message: 'El número del programa debe ser único.'});
+    Program.validatesUniquenessOf('description', {message: 'El nombre del programa debe ser único.'});
 
 };
