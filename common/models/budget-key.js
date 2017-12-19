5b599ad37e871e7c3a70810901147a33b60c8bc7
Brandon Villa <bornofos@gmail.com>
2017-12-18 19:28:34 -0600

    Bug fixed

--- common/models/budget-key.js
+++ common/models/budget-key.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = Budgetkey => {
-    Budgetkey.validatesLengthOf('budget_key_id', {min: 15, message: {min: 'La clave presupuestal es muy larga.'}});
+    Budgetkey.validatesLengthOf('budget_key_id', {max: 15, message: {min: 'La clave presupuestal es muy larga.'}});
     Budgetkey.validatesUniquenessOf('budget_key_id', {message: 'La clave presupuestal debe ser unica.'});
 
 };
