ccf792801357bd8b8c9790b5a9b3b41a2068463a
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-18 19:36:21 -0600

    Added more validations (phone, address, email) to the model Provider.

--- common/models/provider.js
+++ common/models/provider.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Provider) {
-    Provider.validatesUniquenessOf('name', {message: 'El nombre del proveedor debe ser único'});
+    Provider.validatesUniquenessOf('name', {message: 'El nombre del proveedor debe ser único.'});
+    Provider.validatesUniquenessOf('phone', {message: 'El teléfono del proveedor debe ser único.'});
+    Provider.validatesUniquenessOf('address', {message: 'La dirección del proveedor debe ser único.'});
+    Provider.validatesUniquenessOf('email', {message: 'El email del proveedor debe ser único.'});
 };
