c02f068021c64747bce30ebb402782979f899611
Brandon Villa <bornofos@gmail.com>
2018-01-08 19:32:13 -0600

    boss and vice-principal properties models separated in one file

---
+++ roles/users/boss.js
@@ -0,0 +1,19 @@
+module.exports = {
+    model: 'Boss_department',
+    roleName: 'bossdepartment',
+    rolDescription: 'Jefe de departamento',
+    users: [{ 
+        id: 1,
+        name: 'Ada Mabel',
+        username: 'adamabel',
+        email: 'adamabel@gmail.com',
+        password: '123qwe'
+    },
+    {
+        id: 2,
+        name: 'Rodolfo',
+        username: 'rodolfo',
+        email: 'rodolfo@gmail.com',
+        password: '123qwe'
+    }]
+}
---
+++ roles/users/vice-principal.js
@@ -0,0 +1,19 @@
+module.exports = {
+    model: 'Vice_principal',
+    roleName: 'viceprincipal',
+    rolDescription: 'Subdirector',
+    users: [{ 
+        id: 3,
+        name: 'Juan Manuel Topete',
+        username: 'jmanueltopete',
+        email: 'jmanueltopete@gmail.com',
+        password: '123qwe'
+    },
+    {
+        id: 4,
+        name: 'Michel',
+        username: 'Michel',
+        email: 'michel@gmail.com',
+        password: '123qwe'
+    }]
+}
