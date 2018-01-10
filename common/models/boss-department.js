8c2e541a6e13671b74382b4350331a03f8164bde
Brandon Villa <bornofos@gmail.com>
2018-01-10 13:03:45 -0600

    The next remote hooks where created: afterRemote(create) beforeRemote(create) afterRemote(login)

--- common/models/boss-department.js
+++ common/models/boss-department.js
@@ -1,17 +1,52 @@
 'use strict';
 
 module.exports = Bossdepartment => {
-    // Falta: 
-    // Crear el modelo Subdirector y crear la relación: 'Subdirector hasOne subdirection'
-    // Elminar del JSON, el campo boss_name del modelo subdirection
-    Bossdepartment.afterRemote('login', (ctx, user, next) => {
-        // Se indica si es BOSS DEPARTMENT o es SUBDIRECTOR
-        // Para que sepa si hara un GET al endpoint 1 o 2
-        // Con el header Authorization y el token
-        // 1: http://localhost:3000/api/Boss_departments/{userId}?filter[include][department]=requisition
-        // 2: http://localhost:3000/api/Subdirector/{userId}?filter[include]=subdirection
+    const app = require('../../server/server');
+    const uuidv1 = require('uuid/v1');
+    require('dotenv').config();
 
-        user.type = 'BOSS DEPARTMENT';
+    /**
+     * Remote hook description:
+     * 'type' property will be added to the instance (user) that will be 
+     * returned in the login request. With 'type' property, the client
+     * application will know which type of user is loggin in
+     */
+    Bossdepartment.afterRemote('login', (ctx, user, next) => {
+        user.type = process.env.BOSS_DEPARTMENT;
+        next();
+    })
+
+    /**
+     * Remote hook description:
+     * After create a new register, the just created register will be added
+     * to bossdepartment role
+     */
+    Bossdepartment.afterRemote('create', (ctx, modelInstance, next) => {
+        const Role = app.models.Role;
+        const RoleMapping = app.models.RoleMapping;
+        Role.findOne({ where: { name: 'bossdepartment' } }, (err, role) => {
+            if ( err ) next( err );
+            if ( role ) {
+                role.principals.create({
+                    principalType: RoleMapping.USER,
+                    principalId: modelInstance.id
+                }, (err, principal) => {
+                    if ( err ) next ( err );
+                    console.log('Principal created: ', principal);
+                    next();
+                })
+            } else console.log(`There's no role matched`)
+        })
+    });
+
+    /**
+     * Remote hook description:
+     * Before create a new register, a unique id (built with uuid library)
+     * will be added to that request. This with the reason of avoid same 
+     * ids between Bossdepartment and Vice_principal models 
+     */
+    Bossdepartment.beforeRemote('create', (ctx, unused, next) => {
+        ctx.req.body.id = uuidv1();
         next();
     })
 };
--- common/models/vice-principal.js
+++ common/models/vice-principal.js
@@ -3,7 +3,23 @@
 module.exports = Viceprincipal => {
     const app = require('../../server/server');
     const uuidv1 = require('uuid/v1');
-    
+    require('dotenv').config();
+    /**
+     * Remote hook description:
+     * 'type' property will be added to the instance (user) that will be 
+     * returned in the login request. With 'type' property, the client
+     * application will know which type of user is loggin in
+     */
+    Viceprincipal.afterRemote('login', (ctx, user, next) => {
+        user.type = process.env.VICE_PRINCIPAL;
+        next();
+    })
+
+    /**
+     * Remote hook description:
+     * After create a new register, the just created register will be added
+     * to viceprincipal role
+     */
     Viceprincipal.afterRemote('create', (ctx, modelInstance, next) => {
         const Role = app.models.Role;
         const RoleMapping = app.models.RoleMapping;
@@ -22,6 +38,12 @@ module.exports = Viceprincipal => {
         })
     });
 
+    /**
+     * Remote hook description:
+     * Before create a new register, a unique id (built with uuid library)
+     * will be added to that request. This with the reason of avoid same 
+     * ids between Bossdepartment and Vice_principal models 
+     */
     Viceprincipal.beforeRemote('create', (ctx, unused, next) => {
         ctx.req.body.id = uuidv1();
         next();
