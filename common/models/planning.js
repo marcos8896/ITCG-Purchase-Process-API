61cb8151995c8de6cb8e2a06a5ddc516b0eaba5a
Brandon Villa <bornofos@gmail.com>
2018-01-11 15:56:16 -0600

    Functions has been documented

--- common/models/planning.js
+++ common/models/planning.js
@@ -2,14 +2,21 @@
 const app = require('../../server/server');
 const { waterfall } = require('async');
 module.exports = Planning => {
-    
+    /**
+     * Add a Boss_department register to a role by its id
+     * 
+     * @param {string} boss_departmentId the id of a Boss_department register
+     * @param cb Callback
+     */
     Planning.addBoss = (boss_departmentId, cb) => {
         const BossDepartment = app.models.Boss_department;
         addUserToRol( BossDepartment, boss_departmentId, 'bossdepartment')
             .then( principal => cb( null, principal ))
             .catch( error => cb( error ))
     }
-
+    /**
+     * Remote method for addBoss
+     */
     Planning.remoteMethod('addBoss', {
         http: { path: '/addBoss/:boss_departmentId', verb: 'post'},
         accepts: { arg: 'boss_departmentId', type: 'string' },
@@ -18,14 +25,21 @@ module.exports = Planning => {
         ]
     })
 
+    /**
+     * Add a Vice_principal register to a role by its id
+     * 
+     * @param {string} vice_principalId the id of a Vice_principal register
+     * @param cb Callback
+     */
     Planning.addVicePrincipal = (vice_principalId, cb) => {
-
         const VicePrincipal = app.models.Vice_principal;
         addUserToRol( VicePrincipal, vice_principalId, 'viceprincipal')
             .then( principal => cb( null, principal ))
             .catch( error => cb( error ))
     }
-
+    /**
+     * Remote method for addVicePrincipal
+     */
     Planning.remoteMethod('addVicePrincipal', {
         http: { path: '/addVicePrincipal/:vice_principalId', verb: 'post'},
         accepts: { arg: 'vice_principalId', type: 'string' },
@@ -34,7 +48,12 @@ module.exports = Planning => {
         ]
     })
 
-
+    /**
+     * Associate an id to a specific role
+     * 
+     * @param {string} rolName name of the role 
+     * @param {string} id id that will be added to the rolName
+     */
     function addRole(rolName, id) {
         const Role = app.models.Role;
         const RoleMapping = app.models.RoleMapping;
@@ -51,7 +70,13 @@ module.exports = Planning => {
         })
     }
 
-    
+    /**
+     * The whole process to add a user to a rol
+     * 
+     * @param {Object} Model Model from Loopback responsible for add the new register
+     * @param {string} id id of the register that will be added in a new rol
+     * @param {string} rolName name of the rol
+     */
     function addUserToRol( Model, id, rolName) {
         const RoleMapping = app.models.RoleMapping;
         return new Promise( (resolve, reject) => {
