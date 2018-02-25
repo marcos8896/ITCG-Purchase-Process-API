bb916c0bfcab18ce97ae7374f8c6fe6a243b7436
Marcos Barrera Del Río <marcos@softwareallies.com>
2018-02-24 19:39:05 -0600

    Fixed #14 - Now the new models are loaded automatically for the migrations.

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,16 +1,69 @@
 var server = require('./server');
 var ds = server.dataSources.posgresql_ds;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role',
-               'Concept', 'Subdirection','Department',
-               'Concept_Requisition',  'Input_output', 
-               'Input_output_details', 'Provider', 'Purchase_order', 
-               'Purchase_order_Requisition', 'Requisition', 'Project', 'Program',
-               'Budget_key', 'Budget_key_details', 'Boss_department', 'Vice_principal',
-               'Planning'
-              ];
-              
-ds.automigrate(lbTables, function(er) {
-  if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
+
+/**
+ * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+ * Run the proper migrations loading automatically all the new custom models.
+ * @returns {*} 
+ */
+function runModelsMigrations() {
+  getNameModelsArray().then( names => {
+
+    //There's no need to add new custom models in the 'lbTables', since they are
+    //being loaded automatically from the models' JSON files.
+    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', ...names];
+  
+    ds.automigrate(lbTables, function(er) {
+      if (er) throw er;
+      console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+      ds.disconnect();
+    });
+  })
+}
+
+runModelsMigrations();
+
+
+/**
+ * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+ * Return a promises which contains all the Loobpack's custom models with 
+ * all its properties on it.
+ * @returns {Promise<Array>} 
+ */
+function getModelsContentFromJSONs() {
+  const readfiles = require('node-readfiles');
+  let arrayModels = [];
+
+  return readfiles('./common/models/', { filter: '*.json' }, (err, filename, contents) => {
+    if (err) throw err;
+
+    let json = {
+      filename,
+      name: JSON.parse(contents).name,
+      properties_seeds: Object.keys(JSON.parse(contents).properties)
+    }
+
+    arrayModels.push(json);
+
+  }).then(() => arrayModels)
+    .catch( error => console.log(error));
+}
+
+
+/**
+ * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+ * Return a promise which contains only the names from all the Loobpack's custom models.
+ * @returns {Promise<Array>} 
+ */
+async function getNameModelsArray() {
+  
+  let names = [];
+  
+  try {
+    names = await getModelsContentFromJSONs().then( models => models.map( model => model.name));
+  } catch (error) {
+    console.log(error);
+  }
+  
+  return names;
+}
