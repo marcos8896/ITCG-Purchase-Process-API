cefd4cf258e6d26b8281b328e26b3acb9b4288b9
Marcos Barrera Del Río <marcos@softwareallies.com>
2018-02-26 16:41:36 -0600

    Added validation to check if the current users' IDs that are about to be created already exist on the datasource. The ones that already exists will be removed from the array that is used to register new users.

--- roles/create-roles-users.js
+++ roles/create-roles-users.js
@@ -1,5 +1,5 @@
 const   app = require('../server/server'),
-        { parallel, each } = require('async'),
+        { parallel, each, waterfall } = require('async'),
         Role = app.models.Role,
         RoleMapping = app.models.RoleMapping,
         bosses = require('./users/boss'),
@@ -23,7 +23,93 @@ const   app = require('../server/server'),
  * @param cb Callback with two parameters (error, result) 
  */
 const createRole = (model, collection, newRole, roleDescription, cb) => {
-    model.create( collection, (err, users) => {
+  console.log('roleDescription: ', JSON.stringify(roleDescription, null, '  '));
+  console.log('newRole: ', JSON.stringify(newRole, null, '  '));
+  console.log('collection: ', JSON.stringify(collection, null, '  '));
+  console.log('model: ', JSON.stringify(model, null, '  '));
+  console.log("En createRole");
+
+  waterfall([
+
+    //Verify if the current model exists on the database.
+    next => {
+      console.log("------EN PRIMER NEXT------")
+      each(collection, (userToRegister, callback) => {
+
+        model.exists(userToRegister.id)
+            .then( exists => {
+              userToRegister.alreadyExists = exists;
+              callback();
+            })
+            .catch( error => next(error));
+        },
+        err => {
+          if(err) return next(error);
+          
+          return next(null, collection)
+        })
+
+    },
+
+    //Create the current model on the database if it does not exists currently.
+    (collectionOfUsers, next) => {
+
+      //Users that do not exists currently on the database.
+      const usersToRegister = collectionOfUsers.filter( user => !user.alreadyExists )
+
+      model.create( usersToRegister, (err, users) => {
+        
+        if ( err )
+          return next( err );
+        
+        return next(null, users);
+
+      });
+    },
+    
+    (users, next) => {
+      console.log("En la siguiente");
+      
+      console.log("Mis users", JSON.stringify(users, null, '  '));
+    }
+
+
+  ],
+
+  error => {
+    if (error)
+      console.log(error);
+
+    //More code.
+
+  }
+  );
+
+
+    
+        // // create the {newRole} role
+        // Role.create({
+        //     name: newRole,
+        //     description: roleDescription
+        // }, (err, role) => {
+        //     if ( err ) cb( err );
+        //     console.log(`Rol: ${newRole} created`)
+        //     each( collection, (user, next) => {
+        //         //make user part of {newRole}
+        //         role.principals.create({
+        //             principalType: RoleMapping.USER,
+        //             principalId: user.id
+        //         }, (err, principal) => {
+        //             if ( err ) next ( err );
+        //             console.log('Principal created: ', principal);
+        //             next();
+        //         })
+        //     }, error => error ? cb( error ) : cb( null, 'Success' ));
+        // });
+
+    /*
+
+      model.create( collection, (err, users) => {
         if ( err ) return cb( err );
         // create the {newRole} role
         Role.create({
@@ -45,6 +131,10 @@ const createRole = (model, collection, newRole, roleDescription, cb) => {
             }, error => error ? cb( error ) : cb( null, 'Success' ));
         });
     });
+
+    */
+
+
 }
 
 // Execute createRole() function
@@ -72,6 +162,6 @@ parallel({
 },
 (error, result) => {
     if ( error ) throw error;
-    console.log('Script succesfully finshed :D')
+    console.log('Script succesfully finished :D')
     process.exit(0);
 })
