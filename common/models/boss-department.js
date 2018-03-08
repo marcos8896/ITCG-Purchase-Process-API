4f7031b2945a6b51049216b715039fa956dcf4b9
Brandon Villa <bornofos@gmail.com>
2018-03-08 17:29:50 -0600

    minimal changes

--- common/models/boss-department.js
+++ common/models/boss-department.js
@@ -1,29 +1,31 @@
 'use strict';
 
 module.exports = Bossdepartment => {
-    require('dotenv').config();
-    const   app = require('../../server/server'),
-            uuidv1 = require('uuid/v1');
-    
-    /**
-     * Remote hook description:
-     * 'type' property will be added to the instance (user) that will be 
-     * returned in the login request. With 'type' property, the client
-     * application will know which type of user is loggin in
-     */
-    Bossdepartment.afterRemote('login', (ctx, user, next) => {
-        user.type = process.env.ROL_BOSS_DEPARTMENT;
-        next();
-    })
+  require('dotenv').config();
+  const
+    app = require('../../server/server'),
+    uuidv1 = require('uuid/v1');
 
-    /**
-     * Remote hook description:
-     * Before create a new register, a unique id (built with uuid library)
-     * will be added to that request. This with the reason of avoid same 
-     * ids between Bossdepartment and Vice_principal models 
-     */
-    Bossdepartment.beforeRemote('create', (ctx, unused, next) => {
-        ctx.req.body.id = uuidv1();
-        next();
-    })
+  /**
+   * Remote hook description:
+   * 'type' property will be added to the instance (user) that will be 
+   * returned in the login request. With 'type' property, the client
+   * application will know which type of user is loggin in
+   */
+  Bossdepartment.afterRemote('login', (ctx, user, next) => {
+    user.type = process.env.ROL_BOSS_DEPARTMENT;
+    next();
+  })
+
+
+  /**
+   * Remote hook description:
+   * Before create a new register, a unique id (built with uuid library)
+   * will be added to that request. This with the reason of avoid same 
+   * ids between Bossdepartment and Vice_principal models 
+   */
+  Bossdepartment.beforeRemote('create', (ctx, unused, next) => {
+    ctx.req.body.id = uuidv1();
+    next();
+  })
 };
