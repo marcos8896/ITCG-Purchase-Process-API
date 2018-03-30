0929d1bfd9727343bb315eed00de6ad2ecc1bf2d
marcos8896 <elyomarcos@gmail.com>
2018-03-30 13:49:43 -0600

    #17 - Added remote hook to register all the PurchaseOrderRequisition related models.

--- common/models/purchase-order.js
+++ common/models/purchase-order.js
@@ -1,9 +1,33 @@
 'use strict';
+const app = require('../../server/server');
 
 module.exports = Purchaseorder => {
 
+  
+  
   //Validations.
 
   //Disable build-in methods.
   Purchaseorder.disableRemoteMethodByName("deleteById", true);
+
+  /**
+   * Remote hook description:
+   * All PurchaseOrderRequisition objects from purchase_order_requisition 
+   * array will be created on the datasource with their proper relationship
+   * with their PurchaseOrder model.
+   */
+  Purchaseorder.afterRemote('create', (ctx, purchaseOrder, next) => {
+
+    const PurchaseOrderRequisition = app.models.Purchase_order_Requisition;
+    const relatedDetails = ctx.req.body.purchase_order_requisition;
+
+    //Added purchaseOrder id to every relatedDetails model to make the
+    //relationship.
+    relatedDetails.forEach( detail => detail.purchase_orderId = purchaseOrder.id )
+    
+    PurchaseOrderRequisition.create(relatedDetails)
+      .then( createdModels => next())
+      .catch( err => next(err));
+  });
+
 };
